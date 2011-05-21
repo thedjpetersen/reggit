96fed8a8ab6cad335ba0470929b8ac877f42002b
David Petersen <david@bruiser.(none)>
2011-05-20 19:53:27 -0700

    Basic schedule/transcript started make_schedule frontend

--- main/views.py
+++ main/views.py
@@ -1,16 +1,17 @@
 from  django.shortcuts import render_to_response
+import simplejson as json
 from django.http import HttpResponseRedirect
 import registration
 # Create your views here.
 
 
 def index(request):
-    if not request.session['rc'] == None:
+    if 'rc' in request.session:
         return HttpResponseRedirect('/main')
     return render_to_response('index.html')
 
 def login(request):
-    if not request.session['rc'] == None:
+    if 'rc' in request.session:
         return HttpResponseRedirect('/main')
     if request.method == 'GET':
         return render_to_response('index.html')
@@ -26,10 +27,47 @@ def login(request):
         return HttpResponseRedirect('/')
 
 def logout(request):
-    request.session['rc'] = None
+    if 'rc' in request.session:
+        del(request.session['rc'])
     return HttpResponseRedirect('/')
 
 def main(request):
+    if not 'rc' in request.session:
+        return HttpResponseRedirect('/')
     rc = request.session['rc']
     return render_to_response('main.html', {'rc':rc})
+
+
+def transcript(request):
+    if not 'rc' in request.session:
+        return HttpResponseRedirect('/')
+
+    rc = request.session['rc']
+    transcript = rc.transcript.grades
+    return render_to_response('transcript/index.html', {'transcript':transcript})
+
+def schedule(request):
+    if not 'rc' in request.session:
+        return HttpResponseRedirect('/')
+
+    rc = request.session['rc']
+    schedule = rc.schedule.current_classes
+    return render_to_response('schedule/index.html', {'schedule':schedule})
+
+
+def make_schedule(request):
+    if not 'rc' in request.session:
+        return HttpResponseRedirect('/')
+
+    if request.method == 'GET':
+        return render_to_response('make_schedule.html')
+    rc = request.session['rc']
+    try:
+        classes = request.POST['classes'].split(', ')
+    except:
+        return render_to_response('make_schedule.html')
+    combinations = rc.make_schedule(classes)
+    combinations_json = json.dumps(combinations)
+
+    return render_to_response('make_schedule.html', {'combinations':combinations, 'json':combinations_json})
 
--- urls.py
+++ urls.py
@@ -3,8 +3,13 @@ from regfe import main
 from django.conf import settings
 
 urlpatterns = patterns('',
+                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
+
                         (r'^$', 'main.views.index'),
                         (r'^login/$', 'main.views.login'),
                         (r'^logout/$', 'main.views.logout'),
                         (r'^main/$', 'main.views.main'),
+                        (r'^transcript/$', 'main.views.transcript'),
+                        (r'^schedule/$', 'main.views.schedule'),
+                        (r'^make_schedule/$', 'main.views.make_schedule'),
 )
