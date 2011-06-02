e32bbe8b7b86184b6fa476a393cc8e5dd538ad26
David Petersen <david@bruiser.(none)>
2011-06-01 17:03:55 -0700

    Fixed scheduler

--- main/views.py
+++ main/views.py
@@ -44,15 +44,15 @@ def scheduler(request):
     if request.method == 'GET':
         return render_to_response('scheduler/index.html')
     regclass = request.session['regclass']
-    try:
-        classes = request.POST['classes'].split(', ')
-        term_year = request.POST['term'] + request.POST['year']
-        results = regclass.make_schedule(classes, term_year)
-        combinations = results['combinations']
-        classes_possible = results['classes_possible']
-        combinations_json = json.dumps(combinations)
-    except:
-        return render_to_response('scheduler/index.html')
+    #try:
+    classes = request.POST['classes'].split(', ')
+    term_year = request.POST['term'] + request.POST['year']
+    results = regclass.make_schedule(classes, term_year)
+    combinations = results['combinations']
+    classes_possible = results['classes_possible']
+    combinations_json = json.dumps(combinations)
+    #except:
+        #return render_to_response('scheduler/index.html')
 
     return render_to_response('scheduler/index.html', {'combinations':combinations, 'json':combinations_json, 'range':range(24), 'classes_possible':classes_possible})
 
