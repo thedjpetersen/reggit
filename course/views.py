ac1b0734ca664d9a1a4eb7ca6957843a61899cfd
Kevin Ngo <ktngo09@gmail.com>
2011-06-04 15:07:25 -0700

    added course search bar, uses new utilities for input checking

--- course/views.py
+++ course/views.py
@@ -6,16 +6,32 @@ import reglib
 
 def index(request):
 
+    if not 'regclass' in request.session:
+        return HttpResponseRedirect('/')
     regclass = request.session['regclass']
 
-    return render_to_response('course/index.html', {})
+    if request.method == 'GET':
+        return render_to_response('course/index.html')
 
-def show(request, department, course_number):
+    course = request.POST['course']
+    course = reglib.utilities.utilities.format_course(course)
+    course_dict = reglib.utilities.utilities.course_to_dep_and_num(course)
+    try:
+        department = course_dict['department']
+        number = course_dict['number']
+        courses = regclass.class_search(course_dict['department'], course_dict['number'])
+    except:
+        return render_to_response('course/index.html', {'course_not_found':course})
+
+    return render_to_response('course/show.html', {'courses': courses}) 
+
+
+def show(request, department, number):
     regclass = request.session['regclass']
 
-    courses = regclass.class_search(department, course_number)
+    courses = regclass.class_search(department, number)
     if courses is None:
         raise Http404 
     
-    return render_to_response('course/show.html', {'courses': courses, 'department': department, 'course_number': course_number})
+    return render_to_response('course/show.html', {'courses': courses})
     
