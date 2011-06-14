e0bb63e0780c6919632368ff6b62a38eb9c56b04
Kevin Ngo <ktngo09@gmail.com>
2011-06-14 01:44:18 -0700

    displays message if empty schedule, has link to next term's schedule from current term's schedule and vice-versa. BOSS

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('schedule.views',
-                      (r'^$', 'index')
-        
-        )
+                      (r'^$', 'index'),
+                      (r'^next_term/$', 'next_term'),
+)
--- schedule/views.py
+++ schedule/views.py
@@ -13,6 +13,29 @@ def index(request):
     schedule = regclass.schedule.current_classes
     schedule_json = json.dumps(schedule)
 
-    return render_to_response('schedule/index.html', {'schedule':schedule, 'range':range(24), 'json':schedule_json})
+    no_courses = False
+    if len(schedule) is 0:
+        no_courses = True
+
+    if no_courses:
+        return render_to_response('schedule/no_course.html', {'current_term': True})
+    return render_to_response('schedule/index.html', {'schedule':schedule, 'range':range(24), 'json':schedule_json, 'current_term': True})
+
+def next_term(request):
+    if not 'regclass' in request.session:
+        return HttpResponseRedirect('/')
+
+    regclass = request.session['regclass']
+    schedule = regclass.next_schedule.current_classes
+    schedule_json = json.dumps(schedule)
+
+    no_courses = False
+    if len(schedule) is 0: 
+        no_courses = True
+    
+    if no_courses:
+        return render_to_response('schedule/no_course.html', {'current_term': False})
+    return render_to_response('schedule/index.html', {'schedule':schedule, 'range':range(24), 'json':schedule_json, 'current_term': False})
+
 
 
