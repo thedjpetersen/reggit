b1f297b6f01066b6e1a380bb43433292f6e4f318
David Petersen <thedjpetersen@gmail.com>
2011-06-14 20:12:19 -0700

    Added middleware

--- middleware/authentication.py
+++ middleware/authentication.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 class AuthenticationMiddleware(object):
 
     def process_request(self, request):
-        if len(request.path_info)>1:
+        if not request.path_info in ['/', '/login/']:
             if 'regclass' not in request.session:
                 return HttpResponseRedirect('/')
         return None
