ec1ad47d3e81dbe3522caaa8c29a6fdf6981c97b
David Petersen <thedjpetersen@gmail.com>
2011-06-14 20:20:45 -0700

    Corrected mistake in the middleware

--- middleware/authentication.py
+++ middleware/authentication.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 class AuthenticationMiddleware(object):
 
     def process_request(self, request):
-        if not request.path_info in ['/', '/login/']:
+        if not request.path_info[:12] == '/site_media/' and not request.path_info in [u'/', u'/login/']:
             if 'regclass' not in request.session:
                 return HttpResponseRedirect('/')
         return None
