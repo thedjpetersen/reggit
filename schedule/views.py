ae838da781b61aec5aa96acaa74235c3fb10e68a
Kevin Ngo <kevin.ngo@oregonstate.edu>
2011-06-18 21:38:30 -0700

    revamped schedule page

--- schedule/views.py
+++ schedule/views.py
@@ -1,13 +1,15 @@
 from  django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 import simplejson as json
-
+from reglib.utilities import utilities
 
 # Create your views here.
 
 def index(request):
     regclass = request.session['regclass']
     schedule = regclass.schedule.current_classes
+    term = utilities.adjust_schedule_term(regclass.schedule.current_term)
+    term = utilities.format_term(term, True)
     schedule_json = json.dumps(schedule)
 
     no_courses = False
@@ -15,12 +17,14 @@ def index(request):
         no_courses = True
 
     if no_courses:
-        return render_to_response('schedule/no_course.html', {'current_term': True})
-    return render_to_response('schedule/index.html', {'schedule':schedule, 'range':range(24), 'json':schedule_json, 'current_term': True})
+        return render_to_response('schedule/no_course.html', {'current_term': True, 'term': term})
+    return render_to_response('schedule/index.html', {'schedule':schedule, 'term':term, 'range':range(24), 'json':schedule_json, 'current_term': True})
 
 def next_term(request):
     regclass = request.session['regclass']
     schedule = regclass.next_schedule.current_classes
+    term = utilities.adjust_schedule_term(regclass.next_schedule.current_term)
+    term = utilities.format_term(term, True)
     schedule_json = json.dumps(schedule)
 
     no_courses = False
@@ -29,7 +33,7 @@ def next_term(request):
     
     if no_courses:
         return render_to_response('schedule/no_course.html', {'current_term': False})
-    return render_to_response('schedule/index.html', {'schedule':schedule, 'range':range(24), 'json':schedule_json, 'current_term': False})
+    return render_to_response('schedule/index.html', {'schedule':schedule, 'term':term, 'range':range(24), 'json':schedule_json, 'current_term': False})
 
 
 
